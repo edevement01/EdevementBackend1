239ee532ac4c404f8622c0b0b69892705d55271e
shoubhik09 <shoubhik09@gmail.com>
2021-10-18 12:38:35 +0530

    First Commit

---
+++ EdevementStartup/src/main/java/com/edevement/startup/EdevementStartupApplication.java
@@ -0,0 +1,13 @@
+package com.edevement.startup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdevementStartupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdevementStartupApplication.class, args);
+	}
+
+}
---
+++ EdevementStartup/src/test/java/com/edevement/startup/EdevementStartup/EdevementStartupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edevement.startup.EdevementStartup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdevementStartupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
